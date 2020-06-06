d7a2c016133d2d47bb3edb72267caa500caad0fa
Edward Xu <Edward.xu1997@gmail.com>
2020-06-06 21:16:36 +0800

    set the frame

--- src/indi/edward/tank/main.java
+++ src/indi/edward/tank/main.java
@@ -1,5 +1,25 @@
 package indi.edward.tank;
 
-public class main {
+import java.awt.Frame;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
+public class main {
+    public static void main(String[] args) {
+        Frame f = new Frame();
+        f.setSize(800, 600);
+        f.setResizable(false);
+        f.setTitle("Tank Wars");
+        f.setVisible(true);
+
+        f.addWindowListener(new WindowAdapter() {
+
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+
+
+    }
 }
